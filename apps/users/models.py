11350fe8411a9564cea6e4ec8956514c08ce6c4b
Toktorov <ktoktorov2005@gmail.com>
2022-10-05 10:33:56 +0600

    Reverse for 'user_setting' with arguments

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,7 +4,7 @@ import uuid
 
 # Create your models here.
 class User(AbstractUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, editable=False)
     profile_image = models.ImageField(
         upload_to = "profile_image/",
         verbose_name="Фотография профиля",
